6441847f4188b89389041bfd3b5e068ab9e37506
TarasProrok <170684@ukr.net>
2022-12-26 16:26:10 +0200

    Task 3

--- WordFreqCounter.java
+++ WordFreqCounter.java
@@ -5,13 +5,10 @@ import java.io.FileReader;
 import java.util.*;
 
 public class WordFreqCounter {
-    public static Map<String, Integer> sortByValue(Map<String, Integer> hashMap) { //function to sort
-        // creating list of entries in HashMap
+    public static Map<String, Integer> sortByValue(Map<String, Integer> hashMap) {
         List<Map.Entry<String, Integer>> list =
                 new LinkedList<>(hashMap.entrySet());
-        // sorting list
         list.sort((o1, o2) -> (o2.getValue()).compareTo(o1.getValue()));
-        // putting data from sorting list to hashmap
         HashMap<String, Integer> temp = new LinkedHashMap<>();
         for (Map.Entry<String, Integer> aa : list) {
             temp.put(aa.getKey(), aa.getValue());
@@ -21,16 +18,12 @@ public class WordFreqCounter {
 
     public static void main(String[] args) throws Exception {
         HashMap<String, Integer> hashMap = new HashMap<>();
-        // creating reader of file
         try (BufferedReader br = new BufferedReader
                 (new FileReader("/Users/mac/Desktop/Java/Module10/src/words.txt"))) {
             String[] words;
-        // reading line of file
             String line = br.readLine();
-            // splitting line on words and putting them in array
             while (line != null) {
                 words = line.split(" ");
-                // putting array in hashmap
                 for (String word : words) {
                     hashMap.merge(word, 1, Integer::sum);
                 }
@@ -38,7 +31,6 @@ public class WordFreqCounter {
             }
         }
         Map<String, Integer> hm1 = sortByValue(hashMap);
-        // printing hashmap
         for (Map.Entry<String, Integer> en : hm1.entrySet()) {
             System.out.println(en.getKey() +
                     " - " + en.getValue());
